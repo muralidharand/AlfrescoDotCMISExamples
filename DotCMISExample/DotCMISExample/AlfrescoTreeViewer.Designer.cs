c151010af13774ed3858b9a4df6e5b4325c2f587
Administrator <treestec@hotmail.com>
2016-01-30 23:04:22 +0530

    File download option added.

--- DotCMISExample/DotCMISExample/AlfrescoTreeViewer.Designer.cs
+++ DotCMISExample/DotCMISExample/AlfrescoTreeViewer.Designer.cs
@@ -29,32 +29,36 @@
         private void InitializeComponent()
             {
             this.treeView1 = new System.Windows.Forms.TreeView();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.folderDetailsGrid = new System.Windows.Forms.DataGridView();
             this.btnView = new System.Windows.Forms.Button();
             this.btnDownload = new System.Windows.Forms.Button();
             this.btnUpload = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.propertiesGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.folderDetailsGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.propertiesGrid)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 58);
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(327, 585);
+            this.treeView1.Size = new System.Drawing.Size(247, 631);
             this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
             // 
-            // dataGridView1
+            // folderDetailsGrid
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(359, 58);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(860, 506);
-            this.dataGridView1.TabIndex = 1;
+            this.folderDetailsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.folderDetailsGrid.Location = new System.Drawing.Point(276, 12);
+            this.folderDetailsGrid.Name = "folderDetailsGrid";
+            this.folderDetailsGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.folderDetailsGrid.Size = new System.Drawing.Size(1179, 275);
+            this.folderDetailsGrid.TabIndex = 1;
             // 
             // btnView
             // 
-            this.btnView.Location = new System.Drawing.Point(359, 620);
+            this.btnView.Location = new System.Drawing.Point(725, 304);
             this.btnView.Name = "btnView";
             this.btnView.Size = new System.Drawing.Size(75, 23);
             this.btnView.TabIndex = 2;
@@ -64,16 +68,17 @@
             // 
             // btnDownload
             // 
-            this.btnDownload.Location = new System.Drawing.Point(467, 620);
+            this.btnDownload.Location = new System.Drawing.Point(833, 304);
             this.btnDownload.Name = "btnDownload";
             this.btnDownload.Size = new System.Drawing.Size(75, 23);
             this.btnDownload.TabIndex = 3;
             this.btnDownload.Text = "Download";
             this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
             // 
             // btnUpload
             // 
-            this.btnUpload.Location = new System.Drawing.Point(579, 620);
+            this.btnUpload.Location = new System.Drawing.Point(945, 304);
             this.btnUpload.Name = "btnUpload";
             this.btnUpload.Size = new System.Drawing.Size(75, 23);
             this.btnUpload.TabIndex = 4;
@@ -84,20 +89,30 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // propertiesGrid
+            // 
+            this.propertiesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.propertiesGrid.Location = new System.Drawing.Point(276, 333);
+            this.propertiesGrid.Name = "propertiesGrid";
+            this.propertiesGrid.Size = new System.Drawing.Size(1179, 310);
+            this.propertiesGrid.TabIndex = 5;
+            // 
             // AlfrescoTreeViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1231, 646);
+            this.ClientSize = new System.Drawing.Size(1481, 646);
+            this.Controls.Add(this.propertiesGrid);
             this.Controls.Add(this.btnUpload);
             this.Controls.Add(this.btnDownload);
             this.Controls.Add(this.btnView);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.folderDetailsGrid);
             this.Controls.Add(this.treeView1);
             this.Name = "AlfrescoTreeViewer";
             this.Text = "Alfresco Tree Viewer";
             this.Load += new System.EventHandler(this.AlfrescoTreeViewer_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.folderDetailsGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.propertiesGrid)).EndInit();
             this.ResumeLayout(false);
 
             }
@@ -105,10 +120,11 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView folderDetailsGrid;
         private System.Windows.Forms.Button btnView;
         private System.Windows.Forms.Button btnDownload;
         private System.Windows.Forms.Button btnUpload;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView propertiesGrid;
         }
     }
--- DotCMISExample/DotCMISExample/AlfrescoTreeViewer.cs
+++ DotCMISExample/DotCMISExample/AlfrescoTreeViewer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,17 @@ using System.Windows.Forms;
 using DotCMIS;
 using DotCMIS.Client;
 using DotCMIS.Client.Impl;
+using DotCMIS.Data;
+using DotCMIS.Data.Impl;
 
 namespace DotCMISExample
     {
     public partial class AlfrescoTreeViewer : Form
         {
+        private string alfrscoServerURL = "http://localhost:2020/";
+        private string userName = "admin";
+        private string password = "admin";
+        ISession session = null;
         public AlfrescoTreeViewer()
             {
             InitializeComponent();
@@ -22,18 +29,20 @@ namespace DotCMISExample
 
         private void AlfrescoTreeViewer_Load(object sender, EventArgs e)
             {
+            this.WindowState = FormWindowState.Maximized;
             }
 
         private void btnView_Click(object sender, EventArgs e)
             {
 
+
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters[DotCMIS.SessionParameter.BindingType] = BindingType.AtomPub;
-            parameters[DotCMIS.SessionParameter.AtomPubUrl] = "http://localhost:8080/alfresco/api/-default-/public/cmis/versions/1.0/atom";
-            parameters[DotCMIS.SessionParameter.User] = "admin";
-            parameters[DotCMIS.SessionParameter.Password] = "admin";
+            parameters[DotCMIS.SessionParameter.AtomPubUrl] =  alfrscoServerURL + "alfresco/api/-default-/public/cmis/versions/1.0/atom";
+            parameters[DotCMIS.SessionParameter.User] = userName;
+            parameters[DotCMIS.SessionParameter.Password] = password;
             SessionFactory factory = SessionFactory.NewInstance();
-            ISession session = factory.GetRepositories(parameters)[0].CreateSession();
+              session = factory.GetRepositories(parameters)[0].CreateSession();
             IFolder rootFolder = session.GetRootFolder();
 
             //IFolder rootFolder = session.GetObject(rootFolderId) as IFolder;
@@ -52,9 +61,12 @@ namespace DotCMISExample
                 try
                     {
                         IFolder folder = cmisObject as IFolder;
-                        TreeNode currentNode = node.Nodes.Add(cmisObject.Name);
-                        Application.DoEvents();
-                        AddTreeNode(folder, currentNode);  
+                        if (folder != null)
+                        {
+                            TreeNode currentNode = node.Nodes.Add(cmisObject.Id, cmisObject.Name);
+                            Application.DoEvents();
+                            AddTreeNode(folder, currentNode);
+                        }
 
 
                     }
@@ -62,6 +74,60 @@ namespace DotCMISExample
                     {
 
                     }
+                }
+            }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+            {
+                if (e.Node.Text != "Alfresco Root")
+                {
+
+                    string path = e.Node.FullPath.Replace("Alfresco Root", "");
+                    IFolder selectedFolder = session.GetObject(e.Node.Name) as IFolder;
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("Id");
+
+                    DataRow documentRow = null;
+                         
+                    foreach (ICmisObject cmisObject in selectedFolder.GetChildren())
+                    {
+                    if (cmisObject.GetType() != typeof(DotCMIS.Client.Impl.Folder))
+                        { 
+                        documentRow = dt.NewRow();
+                        documentRow["Name"] = cmisObject.Name;
+                        documentRow["Id"] = cmisObject.Id;
+                        dt.Rows.Add(documentRow);
+                        }
+                    }
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        folderDetailsGrid.AutoGenerateColumns = true;
+                        folderDetailsGrid.DataSource = dt;
+                        folderDetailsGrid.Refresh();
+                    }
+                }
+            }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            var currentRow = folderDetailsGrid.SelectedRows[0];
+            string fileName = Convert.ToString(currentRow.Cells[0].Value);
+            string objectId = Convert.ToString(currentRow.Cells[1].Value);
+            try
+                {
+
+                    Document doc = session.GetObject(objectId) as Document;
+                    ContentStream contentStream = (DotCMIS.Data.Impl.ContentStream)doc.GetContentStream();
+                    using (var fileStream = File.Create(@".\" + contentStream.FileName))
+                    {
+                        contentStream.Stream.CopyTo(fileStream);
+                    }
+                    MessageBox.Show("File " + contentStream.FileName + " downloaded successfully");
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("File " + fileName + " download failed." + ex.Message);
                 }
             }
         }

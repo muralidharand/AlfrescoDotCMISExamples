f218c458d3dc8de56f37cc9d925662628f20e9f1
Administrator <treestec@hotmail.com>
2016-02-01 14:48:28 +0530

    Folder property displayed in the grid.

--- DotCMISExample/DotCMISExample/AlfrescoTreeViewer.cs
+++ DotCMISExample/DotCMISExample/AlfrescoTreeViewer.cs
@@ -21,7 +21,11 @@ namespace DotCMISExample
         private string alfrscoServerURL = "http://localhost:2020/";
         private string userName = "admin";
         private string password = "admin";
+        private string ALFRESCO_ROOT = "Alfresco Root";
+        private string CMIS_V1_URL = "alfresco/api/-default-/public/cmis/versions/1.0/atom";
+
         ISession session = null;
+
         public AlfrescoTreeViewer()
             {
             InitializeComponent();
@@ -38,34 +42,29 @@ namespace DotCMISExample
 
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters[DotCMIS.SessionParameter.BindingType] = BindingType.AtomPub;
-            parameters[DotCMIS.SessionParameter.AtomPubUrl] =  alfrscoServerURL + "alfresco/api/-default-/public/cmis/versions/1.0/atom";
+            parameters[DotCMIS.SessionParameter.AtomPubUrl] = alfrscoServerURL + CMIS_V1_URL;
             parameters[DotCMIS.SessionParameter.User] = userName;
             parameters[DotCMIS.SessionParameter.Password] = password;
             SessionFactory factory = SessionFactory.NewInstance();
-              session = factory.GetRepositories(parameters)[0].CreateSession();
+            session = factory.GetRepositories(parameters)[0].CreateSession();
             IFolder rootFolder = session.GetRootFolder();
-
-            //IFolder rootFolder = session.GetObject(rootFolderId) as IFolder;
-
-            // list all children
-
-            TreeNode rootNode = treeView1.Nodes.Add("Alfresco Root");
+            TreeNode rootNode = treeView1.Nodes.Add(ALFRESCO_ROOT);
             AddTreeNode(rootFolder, rootNode);
             }
 
-        private void AddTreeNode(IFolder rootFolder,TreeNode node)
+        private void AddTreeNode(IFolder rootFolder, TreeNode node)
             {
             foreach (ICmisObject cmisObject in rootFolder.GetChildren())
                 {
 
                 try
                     {
-                        IFolder folder = cmisObject as IFolder;
-                        if (folder != null)
+                    IFolder folder = cmisObject as IFolder;
+                    if (folder != null)
                         {
-                            TreeNode currentNode = node.Nodes.Add(cmisObject.Id, cmisObject.Name);
-                            Application.DoEvents();
-                            AddTreeNode(folder, currentNode);
+                        TreeNode currentNode = node.Nodes.Add(cmisObject.Id, cmisObject.Name);
+                        Application.DoEvents();
+                        AddTreeNode(folder, currentNode);
                         }
 
 
@@ -79,55 +78,81 @@ namespace DotCMISExample
 
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
             {
-                if (e.Node.Text != "Alfresco Root")
+            if (e.Node.Text != ALFRESCO_ROOT)
                 {
 
-                    string path = e.Node.FullPath.Replace("Alfresco Root", "");
-                    IFolder selectedFolder = session.GetObject(e.Node.Name) as IFolder;
-                    DataTable dt = new DataTable();
-                    dt.Columns.Add("Name");
-                    dt.Columns.Add("Id");
+                string path = e.Node.FullPath.Replace(ALFRESCO_ROOT, "");
+                IFolder selectedFolder = session.GetObject(e.Node.Name) as IFolder;
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Id");
 
-                    DataRow documentRow = null;
-                         
-                    foreach (ICmisObject cmisObject in selectedFolder.GetChildren())
+                DataRow documentRow = null;
+
+                foreach (ICmisObject cmisObject in selectedFolder.GetChildren())
                     {
+                        /* If the current node is a document, then load them into folderDetailsGrid */
                     if (cmisObject.GetType() != typeof(DotCMIS.Client.Impl.Folder))
-                        { 
+                        {
                         documentRow = dt.NewRow();
                         documentRow["Name"] = cmisObject.Name;
                         documentRow["Id"] = cmisObject.Id;
                         dt.Rows.Add(documentRow);
                         }
-                    }
+                    /* If the current node is a folder, then load folder properties */
+                    else
+                        {
 
-                    if (dt.Rows.Count > 0)
+                            var folderProperties = selectedFolder;
+                            DataTable folderPropertiesDt = new DataTable();
+                            List<IProperty> properties = selectedFolder.Properties.ToList();
+                            folderPropertiesDt.Columns.Add("Name");
+                            folderPropertiesDt.Columns.Add("Value");
+                            foreach (IProperty property in properties)
+                                {
+                                DataRow propertyRow = folderPropertiesDt.NewRow();
+                                propertyRow["Name"] = property.LocalName;
+                                propertyRow["Value"] = property.Value;
+                                folderPropertiesDt.Rows.Add(propertyRow);
+                                }
+                            propertiesGrid.AutoGenerateColumns = true;
+                            propertiesGrid.DataSource = folderPropertiesDt;
+                            propertiesGrid.Refresh();
+                            
+                            
+                            
+                        }
+                    }
+                
+
+                if (dt.Rows.Count > 0)
                     {
-                        folderDetailsGrid.AutoGenerateColumns = true;
-                        folderDetailsGrid.DataSource = dt;
-                        folderDetailsGrid.Refresh();
+                    folderDetailsGrid.AutoGenerateColumns = true;
+                    folderDetailsGrid.DataSource = dt;
+                    folderDetailsGrid.Refresh();
                     }
                 }
             }
 
         private void btnDownload_Click(object sender, EventArgs e)
-        {
+            {
             var currentRow = folderDetailsGrid.SelectedRows[0];
             string fileName = Convert.ToString(currentRow.Cells[0].Value);
             string objectId = Convert.ToString(currentRow.Cells[1].Value);
             try
                 {
 
-                    Document doc = session.GetObject(objectId) as Document;
-                    ContentStream contentStream = (DotCMIS.Data.Impl.ContentStream)doc.GetContentStream();
-                    using (var fileStream = File.Create(@".\" + contentStream.FileName))
+                Document doc = session.GetObject(objectId) as Document;
+                ContentStream contentStream = (DotCMIS.Data.Impl.ContentStream)doc.GetContentStream();
+                using (var fileStream = File.Create(@".\" + contentStream.FileName))
                     {
-                        contentStream.Stream.CopyTo(fileStream);
+                    contentStream.Stream.CopyTo(fileStream);
                     }
-                    MessageBox.Show("File " + contentStream.FileName + " downloaded successfully");
-                }catch(Exception ex)
+                MessageBox.Show("File " + contentStream.FileName + " downloaded successfully");
+                }
+            catch (Exception ex)
                 {
-                    MessageBox.Show("File " + fileName + " download failed." + ex.Message);
+                MessageBox.Show("File " + fileName + " download failed." + ex.Message);
                 }
             }
         }
